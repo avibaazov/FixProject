aab836c383a6e33c46daa9cb55d9d04ce1bf0517
avibaazov <avibaazov2@gmail.com>
2022-12-14 22:50:45 +0200

    innit1

--- app/src/main/java/com/example/fixproject/MainActivity.java
+++ app/src/main/java/com/example/fixproject/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.fixproject;
 
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
+import android.widget.Toast;
 
 
 import com.google.android.material.button.MaterialButton;
@@ -26,7 +31,10 @@ public class MainActivity extends AppCompatActivity {
     private ShapeableImageView asteroid3;
     final int DELAY = 1000;
     private Timer timer = new Timer();
-    private  ShapeableImageView[][] myImages;
+    private ShapeableImageView[][] myImages;
+    private ShapeableImageView[] game_IMG_hearts;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,59 +87,117 @@ public class MainActivity extends AppCompatActivity {
 
     long startTime = 0;
 
+
     private void startTimer() {
         startTime = System.currentTimeMillis();
         timer = new Timer();
-       TimerTask t=new TimerTask() {
-           @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
+        timer.scheduleAtFixedRate(
+                new TimerTask() {
                     @Override
                     public void run() {
-                        for (int i = 0; i < 4; i++) {
 
-//                            Random random=new Random();
-//                            int num=random.nextInt(3);
-                            myImages[i][0].setImageResource(R.drawable.meteor);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                randomlyAddMeteor();
+                                moveMeteor();
+                                checkCollision();
 
-                        }
+                            }
+                        });
                     }
-                });
+                }
+                , 1000, 1000);
+    }
+
+    int strikeCount = 0;
+    //Vibrator vibrator=(Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+    private void checkCollision() {
+        if (this.myImages[3][0].getVisibility() == View.VISIBLE && carPlacementLeft.getVisibility() == View.VISIBLE) {
+            toast("COLLISION!");
+            //vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+            strikeCount++;
+            removeMeteor(myImages[3][0]);
+
+        }
+        if (this.myImages[3][1].getVisibility() == View.VISIBLE && carPlacementCenter.getVisibility() == View.VISIBLE) {
+            toast("COLLISION!");
+          // vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        //vibrator.vibrate(500);
+            strikeCount++;
+            removeMeteor(myImages[3][1]);
+
+        }
+        if (this.myImages[3][2].getVisibility() == View.VISIBLE && carPlacementRight.getVisibility() == View.VISIBLE) {
+            toast("COLLISION!");
+           // vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+
+            strikeCount++;
+
+            removeMeteor(myImages[3][2]);
 
 
+        }
+    if(strikeCount>0&&strikeCount<4) {
+        game_IMG_hearts[game_IMG_hearts.length - strikeCount].setVisibility(View.INVISIBLE);
+    }
+    }
+void removeMeteor(ShapeableImageView img){
+    final Handler handler = new Handler(Looper.getMainLooper());
+    handler.postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            //myImages[3][2].setVisibility(View.INVISIBLE);
+            img.setVisibility(View.INVISIBLE);
+        }
+    }, 300);
 
+}
+    private void toast(String str) {
+
+        Toast
+                .makeText(this, str, Toast.LENGTH_LONG)
+                .show();
+    }
+
+    private void randomlyAddMeteor() {
+        Random rand = new Random();
+        int n = rand.nextInt(3);
+        this.myImages[0][n].setVisibility(View.VISIBLE);
+
+
+    }
+
+    private void moveMeteor() {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (this.myImages[i][j].getVisibility() == View.VISIBLE) {
+
+                    this.myImages[i + 1][j].setVisibility(View.VISIBLE);
+
+                    this.myImages[i][j].setVisibility(View.INVISIBLE);
+
+                    i++;
+                    if(i+1==3){
+                      removeMeteor(myImages[3][j]);
+                  }
+
+                }
             }
-        };
-       timer.scheduleAtFixedRate(t,1000,1000);
-//
-//            timer.scheduleAtFixedRate(
-//                    new TimerTask() {
-//                        @Override
-//                        public void run() {
-//                            runOnUiThread(new Runnable() {
-//                                @Override
-//                                public void run() {
-//                               for (int i = 0; i < 5; i++) {
-////                                    Random random=new Random();
-////                                    int num=random.nextInt(3);
-//                                    myImages[0][i].setImageResource(R.drawable.meteor);
-//                                }
-//                                }
-//                            });
-//                        }
-//                    }
-//                    , DELAY, DELAY);
-
+        }
     }
 
 
     private void findViews() {
-       initViewArray(myImages);
+        initViewArray(myImages);
+        game_IMG_hearts = new ShapeableImageView[]{
+                findViewById(R.id.game_IMG_heart1),
+                findViewById(R.id.game_IMG_heart2),
+                findViewById(R.id.game_IMG_heart3),
 
+        };
         left_BTN = findViewById(R.id.left_BTN);
-        //turn the map invisible
-
-
         right_BTN = findViewById(R.id.right_BTN);
         carPlacementRight = findViewById(R.id.car3);
         carPlacementRight.setVisibility(View.INVISIBLE);
@@ -145,12 +211,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViewArray(ShapeableImageView[][] myImages) {
 
-        this.myImages = new ShapeableImageView[5][3];
+        this.myImages = new ShapeableImageView[4][3];
         this.myImages[0][0] = findViewById(R.id.m1);
 
         this.myImages[0][1] = findViewById(R.id.m2);
         this.myImages[0][2] = findViewById(R.id.m3);
         this.myImages[1][0] = findViewById(R.id.m4);
+        this.myImages[1][1] = findViewById(R.id.m5);
         this.myImages[1][2] = findViewById(R.id.m6);
 
         this.myImages[2][0] = findViewById(R.id.m7);
@@ -159,8 +226,20 @@ public class MainActivity extends AppCompatActivity {
         this.myImages[3][0] = findViewById(R.id.m10);
         this.myImages[3][1] = findViewById(R.id.m11);
         this.myImages[3][2] = findViewById(R.id.m12);
-        this.myImages[4][0] = findViewById(R.id.car1);
-        this.myImages[4][1] = findViewById(R.id.car2);
-        this.myImages[4][2] = findViewById(R.id.car3);
+        setMeteorsInvisible();
+
+//        this.myImages[4][0] = findViewById(R.id.car1);
+//        this.myImages[4][1] = findViewById(R.id.car2);
+//        this.myImages[4][2] = findViewById(R.id.car3);
+
+    }
+
+    private void setMeteorsInvisible() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                this.myImages[i][j].setVisibility(View.INVISIBLE);
+            }
+        }
     }
 }
+
